8eeae0b66b283bd69803bbee28686a6ad1ba6a22
paige-lee <66644787+paige-lee@users.noreply.github.com>
2023-04-09 22:31:32 -0700

    edits

--- Week 1/js/init.js
+++ Week 1/js/init.js
@@ -7,6 +7,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map); // (2)!
 
 //JavaScript let variable declaration to create a marker
-let marker = L.marker([34.0709, -118.444]).addTo(map) // (3)!
-		.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I used to work!')
-		.openPopup();
+//let marker = L.marker([34.0709, -118.444]).addTo(map) // (3)!
+		//.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I used to work!')
+		//.openPopup();
