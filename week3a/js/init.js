db2c14f8e88c520f7ef349e95f0a69db93d6a00b
paige-lee <66644787+paige-lee@users.noreply.github.com>
2023-04-20 15:03:45 -0700

    edits

---
+++ week3a/js/init.js
@@ -0,0 +1,32 @@
+// declare the map and use the variables above
+const map = L.map('the_map').setView([34.0709,-118.444], 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+addMarker(37,-122,'home','home land!')
+addMarker(32,-118,'work','where i work land!')
+addMarker(39,-119,'location 1','random location')
+addMarker(36,-120,'location 2','another random location')
+
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    createButtons(lat,lng,title);
+    return message
+}
+
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+    })
+    document.getElementById("contents").appendChild(newButton); //this adds the button to our page.
+}
+
