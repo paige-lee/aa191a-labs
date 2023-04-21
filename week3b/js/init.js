693e1713edf888cd9985924345bfa422385b6d49
paige-lee <66644787+paige-lee@users.noreply.github.com>
2023-04-20 22:38:53 -0700

    edits

--- week3b/js/init.js
+++ week3b/js/init.js
@@ -1,5 +1,5 @@
 // declare variables
-let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+let mapOptions = {'center': [39.099728, -94.578568],'zoom':4}
 
 // use the variables
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
@@ -12,8 +12,25 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 function addMarker(lat,lng,title,message){
     console.log(message)
     L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    createButtons(lat,lng,title);
     return message
 }
+
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+    })
+    document.getElementById("contents").appendChild(newButton); //this adds the button to our page.
+}
+
+addMarker(40.730610, -73.935242, "New York, NY", "I grew up in the NY area")
+addMarker(34.052235, -118.243683, "Los Angeles, CA", "I came to UCLA for college")
+addMarker(42.361145, -71.057083, "Boston, MA", "I will be moving to Boston after college")
 
 fetch("map.geojson")
     .then(response => {
