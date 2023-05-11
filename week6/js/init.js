58725c5578794fd73b7926f4a210a898ed6d8f67
paige-lee <66644787+paige-lee@users.noreply.github.com>
2023-05-11 14:54:53 -0700

    edits

---
+++ week6/js/init.js
@@ -0,0 +1,50 @@
+// declare variables
+let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+
+// use the variables
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+function addMarker(data){
+    // console.log(data)
+    // these are the names of our lat/long fields in the google sheets:
+    L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h3>Have you ever had COVID-19?</h3> </p>${data["1. Have you ever received a positive COVID-19 diagnosis?"]}</p> <h3>What is your COVID-19 vaccination status? </h3> <p>${data["2. Which of the following options describes your COVID-19 vaccination status?"]}</p>`)
+    createButtons(data.lat,data.lng,data["3. Where do you currently live?"])
+    return
+}
+
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng], 12); //this is the flyTo from Leaflet
+    })
+    const spaceForButtons = document.getElementById('placeForButtons')
+    spaceForButtons.appendChild(newButton);//this adds the button to our page.
+}
+
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSUqE0lgJjHaj4wL7Nty9SnrqA6nEDfKnaG7dEDKEGCCysuDRVjZLBcEWLAs2W6OeDhQ7PoJElstJ3U/pub?output=csv"
+
+function loadData(url){
+    Papa.parse(url, {
+        header: true,
+        download: true,
+        complete: results => processData(results)
+    })
+}
+
+function processData(results){
+    console.log(results)
+    results.data.forEach(data => {
+        console.log(data)
+        addMarker(data)
+    })
+}
+
+loadData(dataUrl)
